1a5dea854f4ad2893563052c3bf0d63a1911abbe
hexaalif <shadullahsad9@gmail.com>
2024-01-30 01:33:04 +0600

    models creating

--- account/models.py
+++ account/models.py
@@ -1,3 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User 
 # Create your models here.
+class UserAccount(models.Model):
+    user = models.OneToOneField(User, related_name='account', on_delete=models.CASCADE)
